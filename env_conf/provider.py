48244ad53f8cdf792596828dbae177a25b656fbc
QuantumGhost <obelisk.reg+github@gmail.com>
2017-07-25 23:23:24 +0800

    Refactoring code
    Add `__str__` and `__repr__` to `Field`
    Change `EnvProvider._load` to `EnvProvider.load`
    Add `eager` argument to control lazy loading behavoir.
    Intialize `FieldType` class passed to `Field` in `Field.__init__`.

--- env_conf/provider.py
+++ env_conf/provider.py
@@ -8,12 +8,20 @@ class Field:
     def __init__(
             self, type_, desc=None, name=None,
             optional=False, default=None):
-        self.type_ = type_
         self.name = name
         self.desc = desc
         self.optional = optional
         self.default = default
         self._cache = None
+        if isinstance(type_, type):
+            if issubclass(type_, FieldType):
+                self.type_ = type_()
+            else:
+                raise TypeError("Invalid field type for {}.".format(self.name))
+        elif isinstance(type_, FieldType):
+            self.type_ = type_
+        else:
+            raise TypeError("Invalid value for {}.".format(self.name))
 
     def resolve(self, env):
         if self._cache is not None:
@@ -25,52 +33,50 @@ class Field:
             elif self.optional:
                 return None
             else:
-                raise Missing("")
-        if isinstance(self.type_, type):
-            if issubclass(self.type_, FieldType):
-                type_ = self.type_()
-            else:
-                raise ValueError("Invalid field type.")
-        elif isinstance(self.type_, FieldType):
-            type_ = self.type_
-        else:
-            raise ValueError("Invalid value")
-        self._cache = type_.parse(value)
+                raise Missing("Required variable {} not set".format(self.name))
+        self._cache = self.type_.parse(value)
         return self._cache
 
     def __get__(self, instance, owner):
         if instance is None:
             return self
-        return self.resolve(owner.env)
+        return self.resolve(instance.env)
+
+    def __str__(self):
+        return "<Field(name={}, type={})>".format(self.name, self.type_)
+
+    def __repr__(self):
+        return str(self)
 
 
 class ProviderMeta(type):
     def __new__(mcs, name, bases, attrs):
         fields = []
-        for k, v in attrs.items():
-            if isinstance(v, Field):
-                if v.name is None:
-                    v.name = k
-                fields.append(v)
-        attrs['_fields'] = fields
+        for key, value in attrs.items():
+            if isinstance(value, Field):
+                if value.name is None:
+                    value.name = key
+                fields.append(value)
+        attrs['fields'] = tuple(fields)
         return super().__new__(mcs, name, bases, attrs)
 
 
 class EnvProvider(metaclass=ProviderMeta):
-    def __init__(self):
-        self._load()
+    def __init__(self, eager=True):
+        if eager:
+            self.load()
 
     @property
     def env(self):
         return os.environ
 
-    def _load(self):
-        for i in self._fields:
+    def load(self):
+        for i in self.fields:   # pylint: disable=no-member
             i.resolve(self.env)
 
     def __str__(self):
         frags = []
-        for i in self._fields:
-            item = '{}={}'.format(i.name, i.resolve(self.env))
+        for i in self.fields:   # pylint: disable=no-member
+            item = '{}={}'.format(i.name, repr(i.resolve(self.env)))
             frags.append(item)
-        return '<Env()>'.format(', '.join(frags))
+        return '<Env({})>'.format(', '.join(frags))
